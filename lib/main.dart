9a5f8dc0fc12162373752441f816d3cdf28b6bf6
RubenCano21 <ruben217.rc@gmail.com>
2026-05-04 13:37:21 -0400

    Update app title and remove debug banner in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Depp Guard',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Depp Guard'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
